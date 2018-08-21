1eb6b1fdaf8daf1e5fcdb1dc34b88cf7a26af22f
Shyam Thiagarajan <shyam.thiagarajan@gmail.com>
2018-08-21 18:44:49 -0400

    Removed Crappy Login

--- app.py
+++ app.py
@@ -2,38 +2,31 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
+"""TODO NEED LOGIN HERE"""
+
+
+
+""" This is Core Funx """
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-def make_new_user(username, password):
-    print(username, password)
+def landing():
     return redirect(url_for('welcome'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return make_new_user(username, password)
-    return render_template('register.html')
-
-@app.route('/login')
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-
-
 
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+""" Redirects from Users Search to show_stock"""
+@app.route('/show/')
+def redir_show():
+    ticker = request.args.get('ticker')
+    return redirect('/show/' + ticker)
+
+""" Shows Stock data for <ticker>
+Params:
+    ticker: ticker value for stock """
+@app.route('/show/<ticker>')
+def show_stock(ticker):
+    return render_template('show_stock.html', SYMBOL=ticker)
 
 if __name__ == '__main__':
     app.run()
