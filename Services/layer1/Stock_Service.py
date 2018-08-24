e32986f1367bfb52c0d3cc5ef1af1bbd91cdda48
Shyam Thiagarajan <shyam.thiagarajan@gmail.com>
2018-08-23 20:19:09 -0400

    Added Services Component

---
+++ Services/layer1/Stock_Service.py
@@ -0,0 +1,15 @@
+
+""" Service involving stock data """
+class StockService():
+    def __init__(self):
+        return
+
+
+    """ Gets the stock price for ticker
+    Args"
+        ticker: stock ticker
+    returns:
+        price of stock"""
+    def get_stock_price(self, ticker):
+        stock_price = 77.7
+        return stock_price
--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, redirect, url_for, request
+from Services.layer1.Stock_Service import StockService
+
+""" declare services """
+stock_service = StockService()
+
 app = Flask(__name__)
 
 
-"""TODO NEED LOGIN HERE"""
-
+"""TODO:CODY NEED LOGIN HERE"""
 
 
 """ This is Core Funx """
-"""Welcome Page to view portfolio and stocks"""
+
+
+"""
+Welcome Page to view portfolio and stocks
+"""
 @app.route('/')
 def landing():
     return redirect(url_for('welcome'))
@@ -16,37 +24,40 @@ def welcome():
     return render_template('welcome.html')
 
 """ Redirects from Users Search to show_stock"""
-@app.route('/show/')
+@app.route('/stocks/show/')
 def redir_show():
     ticker = request.args.get('ticker')
-    return redirect('/show/' + ticker + '/')
+    return redirect('/stocks/show/' + ticker + '/')
 
 """ Shows Stock data for <ticker>
 Params:
     ticker: ticker value for stock """
-@app.route('/show/<ticker>/')
+@app.route('/stocks/show/<ticker>/')
 def show_stock(ticker):
-    return render_template('show_stock.html', SYMBOL=ticker)
+    return render_template('show_stock.html', ticker=ticker, stock_price=stock_service.get_stock_price(ticker))
 
 
 
 
 
-""" Buy and Sell Stocks """
-
+""" 
+Buy and Sell Stocks 
+"""
 """ Buys a stock if funds
 Params:
     ticker: ticker value for stock """
-@app.route('/show/<ticker>/Buy/')
+@app.route('/stocks/show/<ticker>/Buy/')
 def buy_stock(ticker):
-    return render_template('buy_stock.html', SYMBOL=ticker)
+    stock_price = stock_service.get_stock_price(ticker)
+    return render_template('buy_stock.html', ticker=ticker, stock_price=stock_price)
 
 """ Sells a stock if funds
 Params:
     ticker: ticker value for stock """
-@app.route('/show/<ticker>/Sell/')
+@app.route('/stocks/show/<ticker>/Sell/')
 def sell_stock(ticker):
-    return render_template('sell_stock.html', SYMBOL=ticker)
+    stock_price = stock_service.get_stock_price(ticker)
+    return render_template('sell_stock.html', ticker=ticker, stock_price=stock_price)
 
 if __name__ == '__main__':
     app.run()
