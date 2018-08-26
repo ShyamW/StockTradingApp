a242a56ab3162e8585453d75cc4f9b1eff40399a
Porter <porter.850@osu.edu>
2018-08-26 13:52:05 -0400

    added transaction table

--- Models/Model11.py
+++ Models/Model11.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
@@ -13,3 +13,14 @@ class BankWithdrawls(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Transaction(db.Model):
+    person_id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.Integer)
+    stock_ticker = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
+    price_at_transaction = db.Column(db.Decimal)
+
+    def __repr__(self):
+        return self.person_id, self.transaction_id, self.stock_ticker, self.quantity, self.date, self.date, self.price_at_transaction
