7208a784bee1b05d43b46b205f3b511d6b9ec438
Shyam Thiagarajan <shyam.thiagarajan@gmail.com>
2018-08-26 20:26:28 -0400

    removed double Flask

--- Models/Model.py
+++ Models/Model.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
