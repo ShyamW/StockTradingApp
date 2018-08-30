abbdd02b20ce824a25b22e738190612047c7a6fb
np29 <patel.2605@osu.edu>
2018-08-30 18:07:54 -0400

    Added documentation for the portfolio_service.py service.

--- Services/layer1/Portfolio_Service.py
+++ Services/layer1/Portfolio_Service.py
@@ -3,10 +3,23 @@ from Services.layer2 import Stock_Service
 
 
 def _get_cash_available(user):
+    """
+    Gets the balance i
+    Args:
+         user: user for whom you want to get balance for.
+    Returns:
+        The balance of the account for the given user.
+    """
     return user.balance
 
 
 def _get_portfoloio_value(holdings):
+    """
+    Args:
+        holdings:
+    Returns:
+        The portfolio value, total of all the investments.
+    """
     value = 0
     for stock in holdings:
         value = value + stock.quantity * Stock_Service.get_stock_price(stock.stock_ticker)
@@ -14,10 +27,22 @@ def _get_portfoloio_value(holdings):
 
 
 def _get_all_investments(user):
+    """
+    Args:
+         user: user for whom you want to retrieve all the investments for.
+    Returns:
+         all the holdings of the given person, returned as a list.
+    """
     holdings = StockHoldings.query.filter(StockHoldings.person_id == user.id).all()
     return holdings
 
 def get_portfolio(user):
+    """
+    Args:
+        user:
+    Returns:
+        a triplet of bank balance, list of holdings, and the portfolio value as of today.
+    """
     holdings = _get_all_investments(user)
     portfolioValue = _get_portfoloio_value(holdings)
     cash = _get_cash_available(user)
