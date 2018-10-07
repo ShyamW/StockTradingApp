2e06be29e4a3d1f611d6ef5fbdfe8ac39975adae
Cody Sutherland <codymsutherland@gmail.com>
2018-10-06 23:33:08 -0400

    Update Account_Alerts.py

--- Services/layer3/Account_Alerts.py
+++ Services/layer3/Account_Alerts.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC36d4020ee9e2c0ff821a1565d90c856b'
-auth_token = 'fc600177f8717ae72914f73e12be31c7'
+account_sid = 'askinchat'
+auth_token = 'askinchat'
 client = Client(account_sid, auth_token)
 
 
